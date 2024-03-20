3ce17dc03ba8e6bc61463f565949fb8c58763272
latsa <latsapnev01@mail.ru>
2024-03-20 16:34:04 +0300

    Task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(6, 6, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) % 2 != 0 ) {
+                System.out.print(nums.get(i));
+            }
+        }
+
     }
 }
